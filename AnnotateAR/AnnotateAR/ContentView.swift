15bdc8cdb51e0144b160eff3bdb70a6b64b8d20d
Tyler Franklin <tyler@tri.ad>
2020-03-29 04:44:31 -0400

    Initialize ios app

---
+++ AnnotateAR/AnnotateAR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnnotateAR
+//
+//  Created by Tyler Franklin on 3/29/20.
+//  Copyright © 2020 Tyler Franklin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
