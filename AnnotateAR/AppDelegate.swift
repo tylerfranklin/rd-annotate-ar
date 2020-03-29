dda6ce5ac5ab2508a48d12cdaa58f2f91e2721d7
Tyler Franklin <tyler@tri.ad>
2020-03-29 19:48:06 -0400

    Add Firebase to AppDelegate

--- AnnotateAR/AppDelegate.swift
+++ AnnotateAR/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Tyler Franklin. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
