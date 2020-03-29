325d8791b8cf0c2d342513c6cc4a6a9406c7a914
Tyler Franklin <tyler@tri.ad>
2020-03-29 19:51:52 -0400

    Install Firebase UI / Google

--- Pods/Target Support Files/FirebaseUI/FirebaseUI-umbrella.h
+++ Pods/Target Support Files/FirebaseUI/FirebaseUI-umbrella.h
@@ -26,6 +26,8 @@
 #import "FUIPrivacyAndTermsOfServiceView.h"
 #import "FUIAuthTableViewCell.h"
 #import "FUIAuthTableHeaderView.h"
+#import "FirebaseGoogleAuthUI.h"
+#import "FUIGoogleAuth.h"
 
 FOUNDATION_EXPORT double FirebaseUIVersionNumber;
 FOUNDATION_EXPORT const unsigned char FirebaseUIVersionString[];
