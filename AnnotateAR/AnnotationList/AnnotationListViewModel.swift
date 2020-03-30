99521f56dda38eb461e9792c603775b931bfff9b
Tyler Franklin <get@tylerfranklin.com>
2020-03-29 23:59:35 -0400

    Add AnnotationList view model

---
+++ AnnotateAR/AnnotationList/AnnotationListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AnnotationListViewModel.swift
+//  AnnotateAR
+//
+//  Created by Tyler Franklin on 3/29/20.
+//  Copyright © 2020 Tyler Franklin. All rights reserved.
+//
+
+import Foundation
