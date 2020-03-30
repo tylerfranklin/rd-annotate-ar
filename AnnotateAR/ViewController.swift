24157ea646eb2e7c06468fba7db6b95cca7875af
Tyler Franklin <tyler@tri.ad>
2020-03-29 20:49:04 -0400

    Convert project SwiftUI => UIKit

---
+++ AnnotateAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnnotateAR
+//
+//  Created by Tyler Franklin on 3/29/20.
+//  Copyright © 2020 Tyler Franklin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
