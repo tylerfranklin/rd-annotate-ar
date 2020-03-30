99089bbe535a86c7806bad0b103facca07a5acf6
Tyler Franklin <get@tylerfranklin.com>
2020-03-30 03:31:31 -0400

    Add AnnotationList view model with pubsub

--- AnnotateAR/AnnotationList/AnnotationListViewModel.swift
+++ AnnotateAR/AnnotationList/AnnotationListViewModel.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import CodableFirebase
+
+class AnnotationListViewModel {
+    var annotationsReference: DocumentReference?
+
+    fileprivate var query: Query? {
+        didSet {
+            if let listener = listener {
+                listener.remove()
+                observeQuery()
+            }
+        }
+    }
+
+    private var listener: ListenerRegistration?
+    var documents: [DocumentSnapshot] = []
+    var didChangeData: ((AnnotationListViewData) -> Void)?
+
+    var viewData: AnnotationListViewData {
+        didSet {
+            didChangeData?(viewData)
+        }
+    }
+
+    init(viewData: AnnotationListViewData) {
+        self.viewData = viewData
+        query = baseQuery()
+    }
+
+    func observeQuery() {
+        guard let query = query else { return }
+        stopObserving()
+
+        listener = query.addSnapshotListener { [unowned self] (snapshot, error) in
+
+            guard let snapshot = snapshot else {
+                print("Error fetching snapshot results: \(error!)")
+                return
+            }
+
+            let models = snapshot.documents.compactMap { (document) -> Annotation? in
+                do {
+                    let model = try FirestoreDecoder().decode(Annotation.self, from: document.data())
+                    return model
+                } catch {
+                    print("error parsing document: \(document.data())")
+                    return nil
+                }
+            }
+            self.viewData.annotations = models
+            self.documents = snapshot.documents
+        }
+    }
+
+    func stopObserving() {
+        listener?.remove()
+    }
+
+    fileprivate func baseQuery() -> Query {
+        return Firestore.firestore().collection("annotations").limit(to: 50)
+    }
+}
