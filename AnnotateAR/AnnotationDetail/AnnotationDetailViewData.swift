cdb13ca7635a87e585448936051691ea0ba1841b
Tyler Franklin <get@tylerfranklin.com>
2020-03-30 04:16:14 -0400

    Add AnnotationDetail view data file

---
+++ AnnotateAR/AnnotationDetail/AnnotationDetailViewData.swift
@@ -0,0 +1,9 @@
+//
+//  AnnotationDetailViewData.swift
+//  AnnotateAR
+//
+//  Created by Tyler Franklin on 3/30/20.
+//  Copyright © 2020 Tyler Franklin. All rights reserved.
+//
+
+import Foundation
