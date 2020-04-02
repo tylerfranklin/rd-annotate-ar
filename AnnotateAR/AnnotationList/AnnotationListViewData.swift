f6e9eb6af5325f80871c255ecda16fdd12fa81b6
Tyler Franklin <get@tylerfranklin.com>
2020-04-01 21:11:39 -0400

    Abstract annotation model

--- AnnotateAR/AnnotationList/AnnotationListViewData.swift
+++ AnnotateAR/AnnotationList/AnnotationListViewData.swift
@@ -7,40 +7,6 @@
 //
 
 import Foundation
-import FirebaseFirestore
-
-protocol DocumentSerializable {
-    init?(dictionary: [String: Any])
-}
-
-struct Annotation: Codable {
-    var owner: String
-    var place: String
-    var timestamp: Date
-    var title: String
-
-    var dictionary: [String: Any] {
-        return [
-            "owner": owner,
-            "place": place,
-            "timestamp": timestamp,
-            "title": title
-        ]
-    }
-}
-
-extension Annotation: DocumentSerializable {
-
-    init?(dictionary: [String : Any]) {
-        guard let owner = dictionary["owner"] as? String,
-            let place = dictionary["place"] as? String,
-            let timestamp = dictionary["timestamp"] as? Date,
-            let title = dictionary["title"] as? String
-            else { return nil }
-
-        self.init(owner: owner, place: place, timestamp: timestamp, title: title)
-    }
-}
 
 struct AnnotationListViewData {
     var annotations: [Annotation]
