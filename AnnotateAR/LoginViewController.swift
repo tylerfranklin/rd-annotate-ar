f1c89426c6ecea9aad58a146a455dcc6b0c8cf67
Tyler Franklin <get@tylerfranklin.com>
2020-04-01 21:09:00 -0400

    Update Login view controller with manual segue

--- AnnotateAR/LoginViewController.swift
+++ AnnotateAR/LoginViewController.swift
@@ -9,33 +9,48 @@
 import FirebaseUI
 import UIKit
 
-class LoginViewController: UIViewController, FUIAuthDelegate {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+enum Keys: String {
+    case deviceToken = "device_token"
+}
 
+class LoginViewController: UIViewController, FUIAuthDelegate {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         if Auth.auth().currentUser != nil {
+            showList()
         } else {
-            guard let authUI = FUIAuth.defaultAuthUI()
-            else { return }
-
-            authUI.delegate = self
-
+            let authUI = FUIAuth.defaultAuthUI()
+            authUI?.delegate = self
             let providers: [FUIAuthProvider] = [
-              FUIGoogleAuth(),
-//              FUIFacebookAuth(),
-              FUIPhoneAuth(authUI: authUI),
+                FUIGoogleAuth(),
             ]
-            authUI.providers = providers
 
-            let authViewController = authUI.authViewController()
+            authUI?.providers = providers
+
+            let authViewController = authUI!.authViewController()
             present(authViewController, animated: true, completion: nil)
         }
     }
 
-    func authUI(_: FUIAuth, didSignInWith _: User?, error _: Error?) {}
+    func authUI(_: FUIAuth, didSignInWith _: AuthDataResult?, error _: Error?) {
+        let deviceToken = UserDefaults.standard.string(forKey: Keys.deviceToken.rawValue)
+        // check that the user doesn't already exist!
+        AnnotationService().saveNewUser(deviceToken: deviceToken, callback: nil)
+        showList()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        guard
+            let viewController = segue.destination as? AnnotationListViewController
+        else { return }
+
+        viewController.viewModel = AnnotationListViewModel(viewData: AnnotationListViewData(annotations: []))
+    }
+
+    private func showList() {
+        performSegue(withIdentifier: "showList", sender: self)
+    }
 }
