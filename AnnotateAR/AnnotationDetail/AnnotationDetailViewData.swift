10438f46efddfad3e8ca7e994e5242ae4f154bac
Tyler Franklin <get@tylerfranklin.com>
2020-04-01 21:16:18 -0400

    Add AnnotationDetail view data definition

--- AnnotateAR/AnnotationDetail/AnnotationDetailViewData.swift
+++ AnnotateAR/AnnotationDetail/AnnotationDetailViewData.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2020 Tyler Franklin. All rights reserved.
 //
 
+import FirebaseFirestore
 import Foundation
+
+struct AnnotationDetailViewData {
+    var annotation: Annotation
+    var annotationReference: DocumentReference
+    var collectors: [User]
+    var isUserACollector: Bool = false
+}
