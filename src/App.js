d5a00568b7cc94a5ff46301e68071fb4ce66ab45
Tyler Franklin <get@tylerfranklin.com>
2020-11-12 23:39:02 -0500

    Add firebase auth wrapper to application

--- src/App.js
+++ src/App.js
@@ -1,88 +1,81 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-
-import * as React from 'react';
-import * as firebase from 'firebase/app';
+import 'rsuite/dist/styles/rsuite-default.css';
+import {Component} from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import firebase from 'firebase/app';
 import 'firebase/auth';
-
-import withFirebaseAuth, { WrappedComponentProps } from 'react-with-firebase-auth';
-
 import firebaseConfig from './firebaseConfig';
+import { Button } from 'rsuite';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-const firebaseAppAuth = firebaseApp.auth();
-
 /** See the signature above to find out the available providers */
-const providers = {
+const firebaseAppAuth = firebaseApp.auth();const providers = {
   googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
 /** providers can be customised as per the Firebase documentation on auth providers **/
-providers.googleProvider.setCustomParameters({
-  hd: 'mycompany.com',
-});
+// providers.googleProvider.setCustomParameters({
+//   hd: 'mycompany.com',
+// });
+// const App = ({
+//   /** These props are provided by withFirebaseAuth HOC */
+//   // signInWithEmailAndPassword,
+//   // createUserWithEmailAndPassword,
+//   signInWithGoogle,
+//   // signInWithFacebook,
+//   // signInWithGithub,
+//   // signInWithTwitter,
+//   // signInAnonymously,
+//   signOut,
+//   // setError,
+//   user,
+//   // error,
+//   loading,
+// }/*: WrappedComponentProps*/) => (
+//   <React.Fragment>
+//     {
+//       user
+//         ? <h1>Hello, {user.displayName}</h1>
+//         : <h1>Log in</h1>
+//     }
 
-/** Create the FirebaseAuth component wrapper */
-const createComponentWithAuth = withFirebaseAuth({
+//     {
+//       user
+//         ? <button onClick={signOut}>Sign out</button>
+//         : <button onClick={signInWithGoogle}>Sign in with Google</button>
+//     }
+
+//     {
+//       loading && <h2>Loading..</h2>
+//     }
+//   </React.Fragment>
+// );
+
+class App extends Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {
+          user
+            ? <button onClick={signOut}>Sign out</button>
+            : <Button appearance="primary" onClick={signInWithGoogle}>Sign in with Google</Button>
+        }
+      </header>
+    </div>
+    );
+  }
+}
+
+export default withFirebaseAuth({
   providers,
   firebaseAppAuth,
-});
-
-const App = ({
-  /** These props are provided by withFirebaseAuth HOC */
-  // signInWithEmailAndPassword,
-  // createUserWithEmailAndPassword,
-  signInWithGoogle,
-  // signInWithFacebook,
-  // signInWithGithub,
-  // signInWithTwitter,
-  // signInAnonymously,
-  signOut,
-  // setError,
-  user,
-  // error,
-  loading,
-}/*: WrappedComponentProps*/) => (
-  <React.Fragment>
-    {
-      user
-        ? <h1>Hello, {user.displayName}</h1>
-        : <h1>Log in</h1>
-    }
-
-    {
-      user
-        ? <button onClick={signOut}>Sign out</button>
-        : <button onClick={signInWithGoogle}>Sign in with Google</button>
-    }
-
-    {
-      loading && <h2>Loading..</h2>
-    }
-  </React.Fragment>
-);
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-/** Wrap it */
-export default createComponentWithAuth(App);
+})(App);
